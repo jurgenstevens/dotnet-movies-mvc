c84925803e3bcdb8fcbd0473708357e2bd9538da
jurgenstevens <jurgen.stevens@yahoo.com>
2024-08-07 15:45:30 -0500

    Add a view for the HelloWorldController Index method.

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee82c2f132abced83c3671f1d91657fd8143a351")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e514bedd3e68b175ffba8c936cd162ce29cab09")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
