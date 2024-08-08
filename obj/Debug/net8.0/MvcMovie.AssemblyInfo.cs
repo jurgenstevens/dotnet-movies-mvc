9dc2709e6231225316865a9495fbb16bfee54568
jurgenstevens <jurgen.stevens@yahoo.com>
2024-08-08 14:47:33 -0500

    Create Welcome.cshtml file and add a loop in the view template that displays Hello NumTimes.

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36c12eadc85b511c4657f55038a16b3901b31d7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef3190ac1f5f11a65291cbf57a64db02ace2ff0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
