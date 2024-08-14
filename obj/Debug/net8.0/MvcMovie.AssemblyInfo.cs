251d2e57ac624322d399b0034f92b8d9399f74bd
jurgenstevens <jurgen.stevens@yahoo.com>
2024-08-13 21:32:08 -0500

    Run the code generator for the Review model to stub up controllers and templates.

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a8dd58fdb4d2f4f3f5194ea07bbffeaa300a4a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cb71a6c669693b406a93babfa79932e26fee280")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
