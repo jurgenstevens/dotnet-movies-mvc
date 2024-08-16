8b0234094f08798c7af9c9791f9c112b8f5ec426
jurgenstevens <jurgen.stevens@yahoo.com>
2024-08-16 14:40:06 -0500

    Include an HttpPost Index method for testing purposes.

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -20,23 +20,29 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-    public async Task<IActionResult> Index(string searchString)
-    {
-        if (_context.Movie == null)
+        public async Task<IActionResult> Index(string searchString)
         {
-            return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+            if (_context.Movie == null)
+            {
+                return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+            }
+
+            var movies = from m in _context.Movie
+                        select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
+            }
+
+            return View(await movies.ToListAsync());
         }
 
-        var movies = from m in _context.Movie
-                    select m;
-
-        if (!String.IsNullOrEmpty(searchString))
+        [HttpPost]
+        public string Index(string searchString, bool notUsed)
         {
-            movies = movies.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
+            return "From [HttpPost]Index: filter on " + searchString;
         }
-
-        return View(await movies.ToListAsync());
-    }
 
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0676c6c20cbe4ef52b8bd93bf16c0cbf0e6f66f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cf60d0c4c25960d01ba1aba402481aed3e5d3aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
