8fedef49136bd15837712e3533eaecf5bd31771a
jurgenstevens <jurgen.stevens@yahoo.com>
2024-08-16 15:28:29 -0500

    Update form tag with the GET method value.

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cf60d0c4c25960d01ba1aba402481aed3e5d3aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b0234094f08798c7af9c9791f9c112b8f5ec426")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
