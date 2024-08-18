8d93b7e531d69b10473d0c4dd6cce96dd5798e8a
jurgenstevens <jurgen.stevens@yahoo.com>
2024-08-17 22:03:03 -0500

    Add a Rating Property to the Movie Model.

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39c03e4d0cecdcee937b2efb1220235dfe8039e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d86d38861aca134a3cb25e12a8863943dd5e7e0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
